75784c0762a392b51caaa7493c7717afeb8aaa7e
xtian <benedictramirez78@gmail.com>
2023-09-09 21:11:30 +0800

    fixed padding for mobile

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import FetchComponent from "@/components/FetchComponent";
 
 export default function Home() {
   return (
-    <main className="w-full flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black">
+    <main className="w-full flex min-h-screen flex-col items-center justify-between p-2 md:p-20 bg-white text-black">
       
       <FetchComponent />
 
